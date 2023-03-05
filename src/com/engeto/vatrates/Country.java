07cc13c592e2aeb3ff16609a52d7504f3d1072d8
Miloslav Matulka <miloslav.matulka@gmail.com>
2023-03-05 18:02:45 +0100

    Create Country class
    Add class that handles attributes and enables creating objects
    of individual EU countries.

---
+++ src/com/engeto/vatrates/Country.java
@@ -0,0 +1,75 @@
+package com.engeto.vatrates;
+
+import java.math.BigDecimal;
+
+public class Country {
+    //region Attributes
+    private String codeOfCountry;
+    boolean hasVatSpecial;
+    private String nameOfCountry;
+    private BigDecimal vatReduced;
+    private BigDecimal vatStandard;
+    //endregion
+
+    public Country(String codeOfCountry, String nameOfCountry,
+                   BigDecimal vatStandard, BigDecimal vatReduced,
+                   boolean hasVatSpecial) {
+        this.codeOfCountry = codeOfCountry;
+        this.nameOfCountry = nameOfCountry;
+        this.vatStandard = vatStandard;
+        this.vatReduced = vatReduced;
+        this.hasVatSpecial = hasVatSpecial;
+    }
+
+    //region Getters and Setters
+    public String getCodeOfCountry() {
+        return codeOfCountry;
+    }
+
+    public void setCodeOfCountry(String codeOfCountry) {
+        this.codeOfCountry = codeOfCountry;
+    }
+
+    public boolean hasVatSpecial() {
+        return hasVatSpecial;
+    }
+
+    public void setVatSpecial(boolean hasVatSpecial) {
+        this.hasVatSpecial = hasVatSpecial;
+    }
+
+    public String getNameOfCountry() {
+        return nameOfCountry;
+    }
+
+    public void setNameOfCountry(String nameOfCountry) {
+        this.nameOfCountry = nameOfCountry;
+    }
+
+    public BigDecimal getVatReduced() {
+        return vatReduced;
+    }
+
+    public void setVatReduced(BigDecimal vatReduced) {
+        this.vatReduced = vatReduced;
+    }
+
+    public BigDecimal getVatStandard() {
+        return vatStandard;
+    }
+
+    public void setVatStandard(BigDecimal vatStandard) {
+        this.vatStandard = vatStandard;
+    }
+    //endregion
+
+    public String getDescription() {
+        return getNameOfCountry() + " (" + getCodeOfCountry() + "): "
+                + getVatStandard() + " %";
+    }
+
+    public String getDescriptionVerbose() {
+        return getNameOfCountry() + " (" + getCodeOfCountry() + "): "
+                + getVatStandard() + " % (" + getVatReduced() + " %)";
+    }
+}
